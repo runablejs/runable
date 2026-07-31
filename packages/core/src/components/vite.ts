17d5339f9cf21fef7bbb6b94634c4a5bbb1cf912
Mamadou Talla DIA <ibntalla@gmail.com>
2026-07-31 05:00:36 +0100

    feat: supprimer les fichiers next.ts et vite.ts

--- packages/core/src/components/vite.ts
+++
@@ -1,5 +0,0 @@
-import unplugin from './index'
-import type { Options } from './types'
-
-export default (options: Options = {}) => unplugin.vite(options)
-export type { ComponentInfo, ComponentResolver, Options } from './types'
