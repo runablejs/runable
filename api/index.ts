2d39b2ec5bd96f87414ff0d52202cf3a9d4f870a
Mamadou Talla DIA <ibntalla@gmail.com>
2026-08-30 12:46:26 +0100

    feat: réorganiser l'importation et changer le répertoire de travail pour l'application Express

--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,9 @@
-export { default } from "../website/express.js";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const websiteDir = resolve(dirname(fileURLToPath(import.meta.url)), "../website");
+process.chdir(websiteDir);
+
+const { default: app } = await import("../website/express.js");
+
+export default app;
